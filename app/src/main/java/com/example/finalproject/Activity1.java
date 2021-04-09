4a114b1204d6a10aab435bf611abecc400154435
Jennifer Cano <jcano2@oxy.edu>
2021-04-08 23:58:16 -0700

    "Create user and login -- still need to clean up the layout and there is a slight error when the email is blank"

--- app/src/main/java/com/example/finalproject/Activity1.java
+++ app/src/main/java/com/example/finalproject/Activity1.java
@@ -2,12 +2,20 @@ package com.example.finalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Activity1 extends AppCompatActivity {
 
@@ -27,49 +35,84 @@ public class Activity1 extends AppCompatActivity {
     private EditText et_createPasswordConfirm;
     private EditText et_email;
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
 
         et_username=findViewById(R.id.editText_username);
-        et_username.setHint("Username");
-        username=et_username.getText().toString();
+        et_username.setHint("Email"); //change to email later
 
         et_password= findViewById(R.id.editText_password);
         et_password.setHint("Password");
-        password = et_password.getText().toString();
 
         button_login=findViewById(R.id.button_login);
 
-        et_createUsername=findViewById(R.id.editText_createUsername);
+       /* et_createUsername=findViewById(R.id.editText_createUsername);
         et_createUsername.setHint("Create username");
-        createUsername=et_createUsername.getText().toString();
-
+        createUsername=et_createUsername.getText().toString(); // not really used right now
+*/
         et_createPassword=findViewById(R.id.editText_createPassword);
         et_createPassword.setHint("Create password");
-        createPassword=et_createPassword.getText().toString();
 
-        et_createPasswordConfirm=findViewById(R.id.editText_createPasswordConfirm);
+    /*    et_createPasswordConfirm=findViewById(R.id.editText_createPasswordConfirm);
         et_createPasswordConfirm.setHint("Confirm Password");
-        createPasswordConfirm=et_createPasswordConfirm.getText().toString();
-
+        createPasswordConfirm=et_createPasswordConfirm.getText().toString(); // add into an info later in the button listener
+*/
         et_email=findViewById(R.id.editText_email);
         et_email.setHint("Enter email");
-        email=et_email.getText().toString();
 
         createAcct = findViewById(R.id.button_createAcct);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        /*@Override
+        public void onStart(){
+            super.onStart();
+
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            updateUI(currentUser);
+        } */
+
+       /* if(mAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(), Activity2.class));
+            finish();
+        } */
 
         button_login.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-                if(checkCredentials(username,password)==true){
-                    launchNextActivity(v);
+                Log.e("taehyung", "value");
+                username = et_username.getText().toString().trim();
+                password = et_password.getText().toString().trim();
+
+                if (TextUtils.isEmpty(username)) {
+                    et_username.setError("Username is required.");
+                    return;
                 }
-                else{
-                    Toast.makeText(Activity1.this, "Incorrect Credentials, try again", Toast.LENGTH_LONG).show();
+                if (TextUtils.isEmpty(password)) {
+                    et_password.setError("Password is required");
+                    return;
                 }
+
+                //if(checkCredentials(username,password)==true){
+                mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Activity1.this, "Logged in Successful", Toast.LENGTH_SHORT).show();
+                            launchNextActivity(v);
+                        }
+                        else{
+                            Toast.makeText(Activity1.this, "Incorrect Credentials, try again", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+            //}
+
             }
         });
 
@@ -77,19 +120,49 @@ public class Activity1 extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-                if(addUser(createUsername, createPassword,createPasswordConfirm,email)==true){
-                    launchNextActivity(v);
+                //if(addUser(createUsername, createPassword,createPasswordConfirm,email)==true){
+                    createPassword = et_createPassword.getText().toString().trim();
+                    email = et_email.getText().toString().trim();
+
+                    if(TextUtils.isEmpty(createPassword)){
+                        et_createPassword.setError("Password is required.");
+                    }
+                    if(TextUtils.isEmpty(email)){
+                        et_email.setError("Email is required"); //if it's empty it wil take you to the main screen which is not what I want
+                    }
+                    if(createPassword.length() < 6){
+                        et_createPassword.setError("Password must be >= 6 characters");
+                    }
+                    mAuth.createUserWithEmailAndPassword(email,createPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(Activity1.this, "User Created", Toast.LENGTH_SHORT).show();
+                                 launchNextActivity(v);
+                            }
+                            else{
+                                Toast.makeText(Activity1.this, "Something went wrong, please try again " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+                    //else{
+                    //Toast.makeText(Activity1.this, "Something went wrong, please try again", Toast.LENGTH_LONG).show();
+                    //}
+                   // }
+                   // launchNextActivity(v);
                 }
-                else{
-                    Toast.makeText(Activity1.this, "Something went wrong, please try again", Toast.LENGTH_LONG).show();
-                }
-            }
+
         });
 
     }
 
     public boolean checkCredentials(String username, String password){
-        //check that the database has this username and password
+        if(TextUtils.isEmpty(username)){
+            et_username.setError("Username is required.");
+        }
+        if(TextUtils.isEmpty(password)){
+            et_password.setError("Password is required");
+        }
         return false;
     }
 
