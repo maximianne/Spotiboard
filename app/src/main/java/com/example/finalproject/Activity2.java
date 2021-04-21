30225a341d6ae7b1b6650425c2ede9c432d1c0e5
Jennifer <jcano2@oxy.edu>
2021-04-20 18:34:59 -0700

    "Spotify"

--- app/src/main/java/com/example/finalproject/Activity2.java
+++ app/src/main/java/com/example/finalproject/Activity2.java
@@ -2,6 +2,7 @@ package com.example.finalproject;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
 
@@ -12,14 +13,26 @@ import com.spotify.android.appremote.api.Connector;
 import com.spotify.android.appremote.api.SpotifyAppRemote;
 import com.spotify.protocol.types.Track;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Activity2 extends AppCompatActivity {
     private Button search;
     private Button history;
     private SearchView searchView;
+    private String searchText;
+    private String CompleteURL;
 
     private static final String CLIENT_ID = "3Nrfpe0tUJi4K4DXYWgMUX";
     private static final String REDIRECT_URI = "http://com.yourdomain.yourapp/callback";
     private SpotifyAppRemote mSpotifyAppRemote;
+
+    private String urltt1 ="https://api.spotify.com/v1/artists/";
+    private String urltt2="/top-tracks";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,56 @@ public class Activity2 extends AppCompatActivity {
         history = findViewById(R.id.button_history);
         searchView = findViewById(R.id.searchView);
 
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchText=searchView.getQuery().toString();
+                if(!searchText.equals("")){
+                    if(!getArtistID(searchText).equals("")){
+                        CompleteURL=makeURLtopTracks(getArtistID(searchText));
+                    }
+                }
+            }
+        });
+
+    }
+
+    private String makeURLtopTracks(String id){
+        return urltt1+id+urltt2;
+    }
+    private String getArtistID(String artist){
+        String id="";
+        try {
+            JSONObject SpotJSON = new JSONObject(loadJSONFromAsset("spotify.json"));
+            JSONArray spotArray = SpotJSON.getJSONArray("spotifyArtists");
+
+            for(int i = 0; i< spotArray.length(); i++){
+                JSONObject artObject = spotArray.getJSONObject(i);
+
+                if(artObject.getString("name").equals(artist)){
+                    id= artObject.getString("id");
+                }
+            }
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return id;
+    }
+    private String loadJSONFromAsset(String filename) {
+        String json = null;
+        try {
+            InputStream is = this.getAssets().open(filename);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
     }
 
     @Override
