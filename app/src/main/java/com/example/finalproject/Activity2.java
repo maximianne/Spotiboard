ed4c9315392d991f968c21174b58597d63f41353
maximianne <mcastaneda2@oxy.edu>
2021-04-22 15:58:28 -0700

    created 3rd activity

--- app/src/main/java/com/example/finalproject/Activity2.java
+++ app/src/main/java/com/example/finalproject/Activity2.java
@@ -1,8 +1,8 @@
 package com.example.finalproject;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
@@ -16,6 +16,7 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
@@ -30,27 +31,24 @@ import retrofit.RetrofitError;
 
 public class Activity2 extends AppCompatActivity {
     private Button search;
-    private Button history;
+    private Button history; //this should be an arraylist taken from the backend of the app
     private SearchView searchView;
     private String searchText;
-
     private SharedPreferences sharedPreferences;
-    private String urltt1 ="https://api.spotify.com/v1/artists/";
-    private String urltt2="/top-tracks?market=US/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
+
         sharedPreferences=this.getSharedPreferences("SPOTIFY", 0);
+
         search = findViewById(R.id.button_search);
         history = findViewById(R.id.button_history);
         searchView = findViewById(R.id.searchView);
 
        SpotifyApi api = new SpotifyApi();
-
-        String token=sharedPreferences.getString("token", "");
-
+       String token=sharedPreferences.getString("token", "");
         OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
             @Override
             public Response intercept(Chain chain) throws IOException {
@@ -61,45 +59,50 @@ public class Activity2 extends AppCompatActivity {
             }
         }).build();
 
-        Log.d("THE TOKEN:", token);
+        //Log.d("THE TOKEN:", token);
 
         api.setAccessToken(token);
         SpotifyService spotify = api.getService();
 
-        search.setOnClickListener(new View.OnClickListener() { //Maxie's code
+        search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent= new Intent(Activity2.this, Activity3.class);
                 searchText=searchView.getQuery().toString();
                 String artistID = getArtistID(searchText);
                     if(!getArtistID(searchText).equals("")){
-
-                        Log.d("artistID:", artistID);
+                        //Log.d("artistID:", artistID);
                         spotify.getArtistTopTrack(artistID, "US", new Callback<Tracks>() {
                             @Override
                             public void success(Tracks tracks, retrofit.client.Response response) {
-                                Log.d("Tracks success",tracks.tracks.toString());
+                                ArrayList<String> toAdd=new ArrayList<>();
+                                for(int i=0;i<0;i++){
+                                    toAdd.add(tracks.tracks.get(i).toString());
+                                }
+                                intent.putExtra("tracks", toAdd);
                             }
-
                             @Override
                             public void failure(RetrofitError error) {
-                                Log.d("Tracks failure", error.toString());
+                                Toast.makeText(Activity2.this, "Artist does not exist", Toast.LENGTH_LONG).show();
                             }
                         });
-
                         spotify.getArtist(artistID, new Callback<Artist>() {
                             @Override
                             public void success(Artist artist, retrofit.client.Response response) {
-                                Log.d("Album success", artist.name);
+                                //Log.d("Album success", artist.name);
+                                intent.putExtra("artistName", artist.name);
+
                             }
                             @Override
                             public void failure(RetrofitError error) {
-                                Log.d("Album failure", error.toString());
+                                Toast.makeText(Activity2.this, "Artist does not exist", Toast.LENGTH_LONG).show();
                             }
                         });
                 }
                 else{
                     Toast.makeText(Activity2.this, "Artist does not exist", Toast.LENGTH_LONG).show();
                }
+                startActivity(intent);
             }
         });
     }
--- app/src/main/java/com/example/finalproject/Activity3.java
+++ app/src/main/java/com/example/finalproject/Activity3.java
@@ -1,9 +1,26 @@
 package com.example.finalproject;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Activity3 extends AppCompatActivity {
 
+    private TextView artistName;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity3);
+
+        Intent intent=getIntent();
+
+        artistName=findViewById(R.id.textView_artistname);
+        artistName.setText(intent.getStringExtra("artistName"));
 
 
+    }
 }
