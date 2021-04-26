a79f649dfda59c4b8933ed6a14f31414bd31c305
maximianne <mcastaneda2@oxy.edu>
2021-04-25 18:10:26 -0700

    Sucessfully connected to Spotify API

--- app/src/main/java/com/example/finalproject/Activity2.java
+++ app/src/main/java/com/example/finalproject/Activity2.java
@@ -3,6 +3,7 @@ package com.example.finalproject;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SearchView;
@@ -16,18 +17,6 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.ArrayList;
-
-import kaaes.spotify.webapi.android.SpotifyApi;
-import kaaes.spotify.webapi.android.SpotifyService;
-import kaaes.spotify.webapi.android.models.Artist;
-import kaaes.spotify.webapi.android.models.Tracks;
-import okhttp3.Interceptor;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import retrofit.Callback;
-import retrofit.RetrofitError;
 
 public class Activity2 extends AppCompatActivity {
     private Button search;
@@ -41,28 +30,10 @@ public class Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity2);
 
-        sharedPreferences=this.getSharedPreferences("SPOTIFY", 0);
-
         search = findViewById(R.id.button_search);
         history = findViewById(R.id.button_history);
         searchView = findViewById(R.id.searchView);
 
-       SpotifyApi api = new SpotifyApi();
-       String token=sharedPreferences.getString("token", "");
-        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
-            @Override
-            public Response intercept(Chain chain) throws IOException {
-                Request newRequest  = chain.request().newBuilder()
-                        .addHeader("Authorization", "Bearer " + token)
-                        .build();
-                return chain.proceed(newRequest);
-            }
-        }).build();
-
-        //Log.d("THE TOKEN:", token);
-
-        api.setAccessToken(token);
-        SpotifyService spotify = api.getService();
 
         search.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,33 +42,8 @@ public class Activity2 extends AppCompatActivity {
                 searchText=searchView.getQuery().toString();
                 String artistID = getArtistID(searchText);
                     if(!getArtistID(searchText).equals("")){
-                        //Log.d("artistID:", artistID);
-                        spotify.getArtistTopTrack(artistID, "US", new Callback<Tracks>() {
-                            @Override
-                            public void success(Tracks tracks, retrofit.client.Response response) {
-                                ArrayList<String> toAdd=new ArrayList<>();
-                                for(int i=0;i<0;i++){
-                                    toAdd.add(tracks.tracks.get(i).toString());
-                                }
-                                intent.putExtra("tracks", toAdd);
-                            }
-                            @Override
-                            public void failure(RetrofitError error) {
-                                Toast.makeText(Activity2.this, "Artist does not exist", Toast.LENGTH_LONG).show();
-                            }
-                        });
-                        spotify.getArtist(artistID, new Callback<Artist>() {
-                            @Override
-                            public void success(Artist artist, retrofit.client.Response response) {
-                                //Log.d("Album success", artist.name);
-                                intent.putExtra("artistName", artist.name);
-
-                            }
-                            @Override
-                            public void failure(RetrofitError error) {
-                                Toast.makeText(Activity2.this, "Artist does not exist", Toast.LENGTH_LONG).show();
-                            }
-                        });
+                        Log.d("artistID:", artistID);
+                       intent.putExtra("artistID", artistID);
                 }
                 else{
                     Toast.makeText(Activity2.this, "Artist does not exist", Toast.LENGTH_LONG).show();
@@ -115,7 +61,6 @@ public class Activity2 extends AppCompatActivity {
 
             for(int i = 0; i< spotArray.length(); i++){
                 JSONObject artObject = spotArray.getJSONObject(i);
-
                 if(artObject.getString("name").equals(artist)){
                     id= artObject.getString("id");
                 }
--- app/src/main/java/com/example/finalproject/Activity3.java
+++ app/src/main/java/com/example/finalproject/Activity3.java
@@ -1,26 +1,94 @@
 package com.example.finalproject;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import kaaes.spotify.webapi.android.SpotifyApi;
+import kaaes.spotify.webapi.android.SpotifyService;
+import kaaes.spotify.webapi.android.models.Artist;
+import kaaes.spotify.webapi.android.models.Tracks;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+
 public class Activity3 extends AppCompatActivity {
 
     private TextView artistName;
+    private SharedPreferences sharedPreferences;
+    private String artistID;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity3);
-
         Intent intent=getIntent();
+        sharedPreferences=this.getSharedPreferences("SPOTIFY", 0);
+        artistID=intent.getStringExtra("artistID");
 
-        artistName=findViewById(R.id.textView_artistname);
-        artistName.setText(intent.getStringExtra("artistName"));
-
-
+        artistName= findViewById(R.id.textView_artistname);
+        setArtistN(artistID);
     }
+
+    public void getArtistTTracks(String id){
+        SpotifyApi api= getSpotifyService();
+        SpotifyService spotify = api.getService();
+        spotify.getArtistTopTrack(artistID, "US", new Callback<Tracks>() {
+            @Override
+            public void success(Tracks tracks, retrofit.client.Response response) {
+                ArrayList<String> toAdd=new ArrayList<>();
+                for(int i=0;i<0;i++) {
+                    toAdd.add(tracks.tracks.get(i).toString());
+                }
+            }
+            @Override
+            public void failure(RetrofitError error) {
+            }
+        });
+    }
+
+    public void setArtistN(String id){
+        SpotifyApi api= getSpotifyService();
+        SpotifyService spotify = api.getService();
+        spotify.getArtist(artistID, new Callback<Artist>() {
+            @Override
+            public void success(Artist artist, retrofit.client.Response response) {
+                Log.d("Artist success", artist.name);
+                String artistN=artist.name;
+                artistName.setText(artistN);
+            }
+            @Override
+            public void failure(RetrofitError error) {
+            }
+        });
+    }
+
+    public SpotifyApi getSpotifyService(){
+        SpotifyApi api = new SpotifyApi();
+        String token=sharedPreferences.getString("token", "");
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
+
+            @Override
+            public Response intercept(Chain chain) throws IOException {
+                Request newRequest  = chain.request().newBuilder()
+                        .addHeader("Authorization", "Bearer " + token)
+                        .build();
+                return chain.proceed(newRequest);
+            }
+        }).build();
+        api.setAccessToken(token);
+        return api;
+    }
+
 }
